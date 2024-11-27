70354d6dc138a1f1c7c2d112f22076a4938b15d1
Tory Alsip <13713268+toryalsip@users.noreply.github.com>
2024-11-26 19:58:56 -0500

    composite component

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,15 +2,20 @@
 const config = {
   stories: ['../src/components/**/*.stories.@(js|jsx)'],
   staticDirs: ['../public'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-   '@storybook/addon-a11y',
+    '@storybook/addon-a11y',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
+
+  docs: {}
 };
 export default config;
